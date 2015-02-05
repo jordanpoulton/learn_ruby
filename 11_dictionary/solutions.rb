11afb84bfdb00d982be25be3ada1576eb7a8c0ea
jordanpoulton <jordan@oxfordentrepreneurs.co.uk>
2015-02-05 15:19:09 +0000

    Solve 11

---
+++ 11_dictionary/solutions.rb
@@ -0,0 +1,34 @@
+class Dictionary
+  def entries
+    @entries ||= {}
+  end
+
+  def add name, value = nil
+    entries[name.keys.first] = name.values.first if name.is_a? Hash
+    entries[name] = value if name.is_a? String
+  end
+
+  def keywords
+    @entries.keys.sort
+  end
+
+  def include? word
+    entries.keys.include? word
+  end
+
+  def find partial_word
+    result = {}
+    entries.each_pair do |key, definition|
+      if key =~ /^#{partial_word}/
+        result[key] = definition
+      end
+    end
+    result
+  end
+
+  def printable
+    entries.map do |key_val|
+      %Q{[#{key_val.first}] "#{key_val.last}"}
+    end.sort.join("\n")
+  end
+end
